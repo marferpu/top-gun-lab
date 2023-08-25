1bf336625fab9e5a12ca609d7df0c0790f58aed0
MARCELA FERNANDEZ <marferpu17@gmail.com>
2023-08-25 21:28:29 +0200

    Create views file & include all models serialized

--- blogTI/views.py
+++ blogTI/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Usuario, Publicacion, Etiqueta, Reposteo, Comentario
+from .serializers import UsuarioSerializer, PublicacionSerializer, EtiquetaSerializer, ReposteoSerializer, ComentarioSerializer
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class PublicacionViewSet(viewsets.ModelViewSet):
+    queryset = Publicacion.objects.all()
+    serializer_class = PublicacionSerializer
+
+class ReposteoViewSet(viewsets.ModelViewSet):
+    queryset = Reposteo.objects.all()
+    serializer_class = ReposteoSerializer
+
+class ComentarioViewSet(viewsets.ModelViewSet):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer
+
+class EtiquetaViewSet(viewsets.ModelViewSet):
+    queryset = Etiqueta.objects.all()
+    serializer_class = EtiquetaSerializer
