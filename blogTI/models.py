c4c01998b8706f387a2e72ebfcfd376da29bd36d
marcela fernández <marferpu17@gmail.com>
2023-09-11 21:46:42 +0200

    updating models, in process yet

--- blogTI/models.py
+++ blogTI/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
+# from PIL import Image
 class CustomUser(AbstractUser):
     country = models.CharField(max_length=100)
     groups = models.ManyToManyField(
@@ -20,18 +22,50 @@ class CustomUser(AbstractUser):
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
     )
+def user_directory_path(instance, filename):
+    return 'blogTI/{0}/{1}'.format(instance.title, filename)
 
 class Publicacion(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    options = (
+        ('borrador', 'Borrador'),
+        ('publicado', 'Publicado')
+    )
+    
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='publicado')
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')
     title = models.CharField(max_length=70, blank=True)
-    content = models.CharField(max_length=1000)
+    content = models.TextField(max_length=1000)
     publication_date = models.DateTimeField(auto_now=True)
     num_reaction = models.PositiveIntegerField(default=0)
     num_repost = models.PositiveIntegerField(default=0)
     num_comments = models.PositiveIntegerField(default=0)
+    # thumbnail = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
+    slug = models.SlugField(max_length=25, unique_for_date='publication_date', unique=True)
+    # published = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=options, default='borrador')
+    objects = models.Manager()
+    postobjects = PostObjects()
+
+    class Meta:
+        ordering = ('-publication_date',)
 
     def __str__(self):
         return "Post title: " + self.title + ", By: " + self.user_id.username
+
+def nueva_url(instance, url=None):
+    slug = slugify(instance.content)
+    if url is not None:
+        slug = url
+    return slug
+
+def url_ok(sender,instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = nueva_url(instance)
+pre_save.connect(url_ok, sender=Publicacion)
 
 class Etiqueta(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
