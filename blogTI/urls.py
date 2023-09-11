be70b6fe20de7f09cf996ef20ee4af796a4223e2
marcela fernández <marferpu17@gmail.com>
2023-09-11 21:47:16 +0200

    updating urls

--- blogTI/urls.py
+++ blogTI/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, PublicacionViewSet, EtiquetaViewSet, ReposteoViewSet, ComentarioViewSet
-from .views import prueba_cache
+from .views import prueba_cache, BlogListView, PostDetailView
 from . import views
 
+app_name="blogTI"
 router = DefaultRouter()
 router.register(r'usuarios', UserViewSet)
 router.register(r'publicaciones', PublicacionViewSet)
@@ -14,10 +15,13 @@ router.register(r'comentarios', ComentarioViewSet)
 urlpatterns = [
     path('', views.home),
     path('', include(router.urls)),
+    path('posts/', BlogListView.as_view()),
+    path('posts/<post_slug>', PostDetailView.as_view()),
+
     path('prueba/', prueba_cache, name='prueba_cache'), #ruta de la caché, http://localhost:8000/blogTI/prueba/
     #path('registrarPublicacion/', views.registrarPublicacion),
-    path('edicionPublicacion/<id>', views.edicionPublicacion),
-    path('editarPublicacion/<id>', views.editarPublicacion),
-    path('eliminarPublicacion/<id>', views.eliminarPublicacion),
+    # path('edicionPublicacion/<id>', views.edicionPublicacion),
+    # path('editarPublicacion/<id>', views.editarPublicacion),
+    # path('eliminarPublicacion/<id>', views.eliminarPublicacion),
     path('login', views.login)
 ]
